c68663591f48e4e15847db254bda2194eb131613
AnnlynBright <annlynbri@gmail.com>
2024-03-15 10:54:31 +0530

    first commit

---
+++ src/component/MovieList.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react'
+import Movie from './Movie'
+import './MovieList.css'
+
+export default function MovieList() {
+    const [movie,setMovie] = useState([])
+    const getMovies = () =>{
+        fetch("https://65f16b93034bdbecc76271e3.mockapi.io/moviapi/movie",{
+            method:"GET"
+        })
+        .then((data) => data.json())
+        .then((mvs)=>setMovie(mvs))
+    }
+
+    useEffect(()=>{
+        getMovies()
+    },[])
+
+  console.log(movie,"ix");
+  return (
+    <div className='movie-list'>
+        {
+            movie.map((list,index)=>(
+                <div key={index}>
+                    <Movie movieTake={list} getMovies={getMovies}/>
+                </div>
+            ))
+        }
+    </div>
+  )
+}
